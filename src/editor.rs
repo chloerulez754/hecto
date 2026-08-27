8a16fa5beb28bba3a674171521fedf1e28195cae
Chloe Davidson <chloerulez935@outlook.com>
2026-08-28 00:19:45 +1000

    use crossterm::event::read instead of stdin().read_line

--- src/editor.rs
+++ src/editor.rs
@@ -1,5 +1,5 @@
+use crossterm::event::{self, Event::Key, KeyCode::Char};
 use crossterm::terminal::{enable_raw_mode, disable_raw_mode};
-use std::io::{self, Read};
 
 pub struct Editor;
 
@@ -11,21 +11,17 @@ impl Editor {
     pub fn run(&self) {
         enable_raw_mode().unwrap();
 
-        for b in io::stdin().bytes() {
-            match b {
-                Ok(b) => {
-                    let c = b as char;
-                    if c.is_control() {
-                        println!("Binary: {0:08b} ASCII: {0:#03}\r", b);
-                    } else {
-                        println!("Binary: {0:08b} ASCII: {0:#03} Character: {1:#?}\r", b, c);
-                    }
+        loop {
+            match event::read() {
+                Ok(Key(event)) => {
+                    println!("{event:?}\r");
 
-                    if c == 'q' {
+                    if let Char('q') = event.code {
                         break;
                     }
                 }
                 Err(err) => println!("Error: {err}"),
+                _ => (),
             }
         }
 
